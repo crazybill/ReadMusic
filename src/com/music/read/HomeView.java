b8088cf040e1dd32093f357b8244163af7b9ebb9
xu panpan <kadisting@gmail.com>
2017-11-07 14:58:02 +0800

    no message

--- src/com/music/read/HomeView.java
+++ src/com/music/read/HomeView.java
@@ -16,7 +16,6 @@ import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.effect.BlendMode;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseButton;
